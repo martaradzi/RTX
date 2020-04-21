26e8142411bc435aefbc0e772971eb8d9e4e4ecc
martaradzi <marta.radziszewska@gmail.com>
2020-04-21 18:31:24 +0200

    new test

--- examples/crowdnav-birchClustering/definition.py
+++ examples/crowdnav-birchClustering/definition.py
@@ -5,7 +5,7 @@ name = "CrowdNav-BirchClustering"
 
 execution_strategy = {
     "ignore_first_n_results": 2000,
-    "sample_size": 4,
+    "sample_size": 8,
     "window_size_for_car_number_change": 3000,
     "partial_clustering_size": 4,
     "save_in": './experiments/start_car_number_test/',
